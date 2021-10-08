ff6cc0220a62a4cf17f4bf7871b4f76bbfbcb387
NirupanJoe <nirupanjoej@gmail.com>
2021-10-08 14:26:49 +0000

    Improved two coloum responsive by grid.

--- src/components/populationTableBody.js
+++ src/components/populationTableBody.js
@@ -1,22 +1,33 @@
 import { React } from 'react';
 import RemoveButton from './removeButton';
 
+// eslint-disable-next-line max-lines-per-function
 const PopulationTableBody = ({
 	id, location, totalPopulation, malePopulation, femalePopulation,
 }) =>
 	<div key={ id } className="population" role="populationTableBody">
-		<div className="attribute" role="location">{location}</div>
+		<div
+			className="attribute"
+			role="location"
+			data-name="Location"
+		>{location}</div>
 		<div
 			className="attribute"
 			role="totalPopulation"
+			data-name="TotalPopulation"
 		>{totalPopulation}</div>
-		<div className="attribute" role="malePopulation">{malePopulation}</div>
+		<div
+			className="attribute"
+			role="malePopulation"
+			data-name="MalePopulation"
+		>{malePopulation}</div>
 		<div
 			className="attribute"
 			role="femalePopulation"
+			data-name="FemalePopulation"
 		>{femalePopulation}</div>
 		<div
-			className="attribute"
+			className="attribute remove-population"
 			role="removePopulation"
 		>{RemoveButton(id)}</div>
 	</div>;
